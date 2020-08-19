1cdb19db6d3c9aa08093e216d657504b8b86ca95
ttnaoto <ttnaoto@gmail.com>
2020-08-19 18:42:57 +0900

    change: detail page を stateful にして、 dispose で保存するようにした

--- lib/ui/page/detail_page/bean_detail_page/bean_detail_page.dart
+++ lib/ui/page/detail_page/bean_detail_page/bean_detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:coffee_life_manager/model/bean.dart';
+import 'package:coffee_life_manager/repository/model/dao/bean_dao_impl.dart';
 import 'package:coffee_life_manager/ui/page/detail_page/detail_page.dart';
 import 'package:coffee_life_manager/ui/page/detail_page/widget/button/fav_button.dart';
 import 'package:coffee_life_manager/ui/page/detail_page/widget/detail_list_tile/detail_datetime_list_tile.dart';
@@ -8,21 +9,32 @@ import 'package:coffee_life_manager/ui/page/detail_page/widget/image_card_widget
 import 'package:coffee_life_manager/ui/page/detail_page/widget/rate_widget/rate_widget.dart';
 import 'package:flutter/material.dart';
 
-class BeanDetailPage extends StatelessWidget {
+class BeanDetailPage extends StatefulWidget {
   const BeanDetailPage(this._bean);
 
   final Bean _bean;
 
   @override
+  _BeanDetailPageState createState() => _BeanDetailPageState();
+}
+
+class _BeanDetailPageState extends State<BeanDetailPage> {
+  @override
+  void dispose() {
+    BeanDaoImpl().insert(widget._bean);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return DetailPage(
       header: DetailHeader(
-        imageInformation: _bean,
+        imageInformation: widget._bean,
         actions: [
           FavButton(
-            isFavorite: _bean.isFavorite,
+            isFavorite: widget._bean.isFavorite,
             onChanged: (val) {
-              _bean.isFavorite = val;
+              widget._bean.isFavorite = val;
             },
           ),
           IconButton(icon: const Icon(Icons.local_cafe), onPressed: () {}),
@@ -36,49 +48,49 @@ class BeanDetailPage extends StatelessWidget {
         DetailIntListTile(
           title: const Text('残量'),
           unit: 'g',
-          initialValue: _bean.remainingAmount,
+          initialValue: widget._bean.remainingAmount,
           onChanged: (val) {
-            _bean.remainingAmount = val;
+            widget._bean.remainingAmount = val;
           },
         ),
         DetailIntListTile(
           title: const Text('一杯当たり'),
           unit: 'g',
-          initialValue: _bean.oneCupPerGram,
+          initialValue: widget._bean.oneCupPerGram,
           onChanged: (val) {
-            _bean.oneCupPerGram = val;
+            widget._bean.oneCupPerGram = val;
           },
         ),
         DetailIntListTile(
           title: const Text('値段'),
           unit: '円',
-          initialValue: _bean.price,
+          initialValue: widget._bean.price,
           onChanged: (val) {
-            _bean.price = val;
+            widget._bean.price = val;
           },
         ),
         RoastListTile(
-          initialValue: _bean.roast,
+          initialValue: widget._bean.roast,
           onChanged: (val) {
-            _bean.roast = val;
+            widget._bean.roast = val;
           },
         ),
         DetailDateTimeListTile(
           title: const Text('賞味期限'),
-          initialValue: _bean.freshnessDate,
+          initialValue: widget._bean.freshnessDate,
           onChanged: (val) {
             // TODO: 賞味期限を変更できるダイアログの追加
           },
         ),
         DetailDateTimeListTile(
           title: const Text('開封日時'),
-          initialValue: _bean.openTime,
+          initialValue: widget._bean.openTime,
           onChanged: (val) {
-            _bean.openTime = val;
+            widget._bean.openTime = val;
           },
         ),
       ],
-      rate: RateWidget(_bean.rate),
+      rate: RateWidget(widget._bean.rate),
       links: [
         ListTile(
           title: const Text('この豆で淹れたコーヒー'),
@@ -94,12 +106,12 @@ class BeanDetailPage extends StatelessWidget {
         ),
       ],
       memo: TextFormField(
-        initialValue: _bean.memo,
+        initialValue: widget._bean.memo,
         decoration: const InputDecoration(
           labelText: 'メモ',
         ),
         onChanged: (val) {
-          _bean.memo = val;
+          widget._bean.memo = val;
         },
         maxLength: 50,
         maxLines: 5,
