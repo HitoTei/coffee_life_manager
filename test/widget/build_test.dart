8f53243bd9f038200e927fd0d4f9b4018a9a3a24
ttnaoto <ttnaoto@gmail.com>
2020-09-01 09:04:31 +0900

    remove: 不要なテストを削除

--- test/widget/build_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:coffee_life_manager/app.dart';
-import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  testWidgets('build test', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
-  });
-}
